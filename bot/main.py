4f22ca0c57da82f08f90447444d659c523a3e3b4
yenashenakhte2016 <mmnsfyrad@gmail.com>
2016-08-08 02:22:47 +0430

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -8,13 +8,13 @@ from database import prepare_index
 
 rest = RestBridge(bot)
 
-async def start():
+async def start()
     await prepare_index()
     await rest.start()
     await bot.loop()
 
 
-async def stop():
+async def stop()
     await rest.stop()
 
 
